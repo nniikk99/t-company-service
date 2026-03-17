63da92e0c8ec8ef14e817ce7b27708104095e0d1
Mikhail <nniikk.9@mail.ru>
2026-03-17 22:49:23 +0300

    Fix: remove incorrect user_role.dart import and resolve compilation error

--- lib/screens/requests_list_screen.dart
+++ lib/screens/requests_list_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../../models/user.dart';
-import '../models/user_role.dart';
 import '../models/service_request.dart';
 import '../services/supabase_service.dart';
 import '../widgets/animated_card.dart';
